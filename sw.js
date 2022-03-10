c03e0cebde1ce423f58ed683540f095cc9bbd891
Abraham Ebijuni <91433818+abrahamebij@users.noreply.github.com>
2022-03-10 13:09:30 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,18 +4,17 @@ self.addEventListener("install", (e) => {
 			.open("fox-store")
 			.then((cache) =>
 				cache.addAll([
-					"/",
-					"/index.html",
-					"/assets/scripts.js",
-					"/assets/styles.css",
-					"/assets/hint.css",
-					"/assets/overview.png",
-					"/assets/favicon/favicon.ico",
-					"/assets/favicon/favicon-16x16.png",
-					"/assets/favicon/favicon-32x32.png",
-					"/assets/favicon/apple-touch-icon.png",
-					"/assets/favicon/android-chrome-192x192.png",
-					"/assets/favicon/android-chrome-512x512.png",
+					"index.html",
+					"assets/scripts.js",
+					"assets/styles.css",
+					"assets/hint.css",
+					"assets/overview.png",
+					"assets/favicon/favicon.ico",
+					"assets/favicon/favicon-16x16.png",
+					"assets/favicon/favicon-32x32.png",
+					"assets/favicon/apple-touch-icon.png",
+					"assets/favicon/android-chrome-192x192.png",
+					"assets/favicon/android-chrome-512x512.png",
 				])
 			)
 	);
